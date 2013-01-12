ff52d8433f3c443769aa9c88e1b270d944ea2986
Xinqian Weng <scutwengxinqian@126.com>
2013-01-12 11:26:29 +0800

    [temp] JTAG TAP can be recognized, but "Burst read timed out."

--- boards/altera/de2_115/syn/quartus/tcl/GENERIC_JTAG_TAP_pin_assignments.tcl
+++ boards/altera/de2_115/syn/quartus/tcl/GENERIC_JTAG_TAP_pin_assignments.tcl
@@ -1,8 +1,16 @@
-set_location_assignment PIN_AF22 -to tck_pad_i ; #Dummy - JTAG[20]
+# use GPIO, locates at lower right conner of the IDE port.
+set_location_assignment PIN_AE24 -to tck_pad_i ; # GPIO[27]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tck_pad_i
-set_location_assignment PIN_AD22 -to tdo_pad_o ; #Dummy - JTAG[21]
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tdo_pad_o
-set_location_assignment PIN_AG25 -to tdi_pad_i ; #Dummy - JTAG[22]
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tdi_pad_i
-set_location_assignment PIN_AD25 -to tms_pad_i ; #Dummy - JTAG[23]
+
+set_location_assignment PIN_AF26 -to tms_pad_i ; # GPIO[29]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tms_pad_i
+
+set_location_assignment PIN_AG23 -to tdi_pad_i ; # GPIO[31]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tdi_pad_i
+
+set_location_assignment PIN_AH26 -to tdo_pad_o ; # GPIO[33]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tdo_pad_o
+
+set_location_assignment PIN_AG26 -to trstn_pad_i ; # GPIO[35]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to trstn_pad_i
+
